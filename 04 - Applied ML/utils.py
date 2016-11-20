caa13e001b9b9fb96091dcc1728c02b256923035
Arbër Salihi <arbersalihi@MacBook-Pro-de-Arber.local>
2016-11-20 19:18:20 +0100

    [Homework 4] Add of ROC AUC (for single RFC), minor changes in part 2, add comments in code and Notebook (not finished yet)

--- 04 - Applied ML/utils.py
+++ 04 - Applied ML/utils.py
@@ -25,6 +25,7 @@ def compute_age(row):
         Given a player, function returns the years of player.
         row: Row of the DataFrame, representing a dyad which contains a player.
     """
+
     data_date = date(2013, 1, 1)
     delta = relativedelta.relativedelta(data_date, row['birthday'])
 
@@ -39,6 +40,7 @@ def regulate_number_of_cards(row, cards_name, k=1):
     cardsName: Type of received cards for the player
     k: factor of impact (more k is bigger more the regulation is important)
     """
+
     nb_cards = row[cards_name]
 
     if row['associationScore'] > 0:
@@ -53,8 +55,38 @@ def regulate_number_of_cards(row, cards_name, k=1):
     return nb_cards
 
 
+def compute_skin_separation_percentage(estimation, real_classes):
+    """
+    This function computes the the separation of players (in different clusters) regarding to their skin color.
+
+    estimation: Represents in which cluster are marked the players
+    real_classes: Represents the skin color of all the players
+    """
+
+    if len(estimation) != len(real_classes):
+        print('The size of the two arrays are not equals')
+        return 0
+    
+    size = len(estimation)
+    equal = 0
+    for i in range(0, size):
+        # 
+        if estimation[i] == real_classes[i]:
+            equal += 1
+    different = size - equal
+    return abs(equal - different) / size
+
+
 def get_random_forests(nb_trees=[10], min_leaf=[2], min_split=[2]):
+    """
+    Given a set of parameters, function create a set of RandomForestClassifier for each unique set of parameters.
+
+    These parameters refer to parameters of RandomForestClassifier's constructor.
+    For more detail, please visit: http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+    """
+
     rfs = []
+
     for tree_param in nb_trees:
         for min_leaf_param in min_leaf:
             for min_split_param in min_split:
@@ -63,10 +95,24 @@ def get_random_forests(nb_trees=[10], min_leaf=[2], min_split=[2]):
 
 
 def run_once(classifiers, data, features, classes):
+    """
+    For each classifier, we run fit/predict operations of the aforesaid classifier.
+
+    classifiers: set of classifiers to be used
+    data: Data to be used for train and test operations
+    features: Features to be considered
+    classes: Classes to be predicted
+    """
+
+    # We keep only interesting features
     x = data[features]
+    # We then use LabelEncoder to transform classes, which are denoted as string by default
     y = preprocessing.LabelEncoder().fit_transform(data[classes])
+    # Train/Test/Split to obtain randomly-generated sets for train and test parts
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=4)
-    y_test_binary = preprocessing.LabelBinarizer().fit_transform(y_test)
+    # We finally binarize the obtained labels to compute ROC AUC later
+    lb = preprocessing.LabelBinarizer()
+    y_test_binary = lb.fit_transform(y_test)
 
     results = []
 
@@ -75,12 +121,14 @@ def run_once(classifiers, data, features, classes):
         predictions = classifier.predict(x_test)
         y_probabilities = classifier.predict_proba(x_test)
 
+        # We store important data relative to the classifier and its results
         result = {
             'classifier': classifier,
             'scores': {
                 'accuracy': metrics.accuracy_score(y_test, predictions),
                 'f-score': metrics.f1_score(y_test, predictions, average='binary'),
-                #'roc_auc': metrics.roc_auc_score(y_test_binary, y_probabilities)
+                'roc': metrics.roc_curve(y_test_binary, y_probabilities[:, 1]),
+                'roc_auc': metrics.roc_auc_score(y_test_binary, y_probabilities[:, 1])
             },
             'confusion_matrix': metrics.confusion_matrix(y_test, predictions)
         }
@@ -91,18 +139,31 @@ def run_once(classifiers, data, features, classes):
 
 
 def run_cross_validation(classifiers, data, features, classes):
+    """
+    For each given classifier, function run cross validation to obtain mean accuracy
+
+    classifiers: Classifiers to be considered for cross validation
+    data: Data to be used with classifiers (train and test)
+    features: Relevant features to be considered
+    classes: Classes to be predicted
+    """
 
     results = []
-    y_binary = preprocessing.LabelBinarizer().fit_transform(data[classes])
+    # We first transform classes to obtain binary values
+    y = preprocessing.LabelEncoder().fit_transform(data[classes])
+    y_binary = preprocessing.LabelBinarizer().fit_transform(y)
 
     for classifier in classifiers:
         accuracy = cross_val_score(classifier, data[features], data[classes], cv=10, scoring='accuracy')
+        f1 = cross_val_score(classifier, data[features], data[classes], cv=10, scoring='f1_macro')
         #roc_auc = cross_val_score(classifier, data[features], y_binary, cv=10, scoring='roc_auc')
 
+        # We store important data relative to classifier and cross validation scores
         result = {
             'classifier': classifier,
             'scores': {
                 'accuracy_mean': np.mean(accuracy),
+                'f1_mean': np.mean(f1)
                 #'roc_auc_mean': np.mean(roc_auc)
             }
         }
@@ -112,116 +173,26 @@ def run_cross_validation(classifiers, data, features, classes):
     return results
 
 
-# Function is defined in sklearn documentation and was slightly modified to fit with our needs and our situation
-# See: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+def retrieve_above_threshold(classifier, features, threshold=10):
     """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
+    Given a classifier, function return features for which importance (relative to the most important feature) is above threshold
+
+    classifier: Classifier to be considered
+    features: Features used with classifier
+    threshold: Threshold used to discriminate features
     """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-
-def retriew_above_thresold(classifier, features, thresold=10):
 
     feature_importance = classifier.feature_importances_
+    # We compute importance score relative to the most important feature used by the classifier
     feature_importance = 100.0 * (feature_importance / feature_importance.max())
 
-    index = np.where(feature_importance > thresold)[0]
+    # We obtain indexes of all features which are the most important (importance score abode threshold)
+    index = np.where(feature_importance > threshold)[0]
 
     return pd.Series(data=[feature_importance[i] for i in index], index=[features[i] for i in index])
 
 
-# From http://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
-def plot_learning_curve(estimator, title, data, features, classes, ylim=None):
-    """
-    Generate a simple plot of the test and training learning curve.
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
-    title : string
-        Title for the chart.
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-          - None, to use the default 3-fold cross-validation,
-          - integer, to specify the number of folds.
-          - An object to be used as a cross-validation generator.
-          - An iterable yielding train/test splits.
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-    n_jobs : integer, optional
-        Number of jobs to run in parallel (default 1).
-    """
-    X = data[features]
-    y = preprocessing.LabelEncoder().fit_transform(data[classes])
-
-    cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-
-    plt.figure()
-    plt.title(title)
-    if ylim is not None:
-        plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5))
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    plt.grid()
-
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-
-    plt.legend(loc="best")
-    return plt
-    
-def get_mean_squared_error(classifier, data, features, classes, samples,nbCrossVal):
-    
+def get_mean_squared_error(classifier, data, features, classes, samples, nbCrossVal):
     error_with_cv_list = []
     error_without_cv_list = []
     
@@ -246,12 +217,13 @@ def get_mean_squared_error(classifier, data, features, classes, samples,nbCrossV
 
         #training part.
         error_with_cv = cross_val_score(classifier, x, y, cv=50, scoring='neg_mean_squared_error')
-        error_without_cv = mean_squared_error(y_test,predictions,multioutput='raw_values')
+        error_without_cv = mean_squared_error(y_test, predictions, multioutput='raw_values')
         
         error_with_cv_list.append(abs(error_with_cv))
         error_without_cv_list.append(error_without_cv)
         
-    return (samples , error_with_cv_list ,error_without_cv_list)
+    return (samples , error_with_cv_list , error_without_cv_list)
+
 
 def learning_curve_mean_squared_error (classifier, data, features, classes, nb_samples,nbCrossVal,ylim = None):
 
@@ -284,6 +256,105 @@ def learning_curve_mean_squared_error (classifier, data, features, classes, nb_s
 
     plt.grid()
 
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+# Function is defined in sklearn documentation and was slightly modified to fit with our needs and the situation
+# See: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+# Function is defined in sklearn documentation
+# See: http://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+def plot_learning_curve(estimator, title, data, features, classes, ylim=None):
+    """
+    Generate a simple plot of the test and training learning curve.
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+    title : string
+        Title for the chart.
+    data : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+    features : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+    /* INTERNAL VARIABLE ONLY HERE */
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - An object to be used as a cross-validation generator.
+          - An iterable yielding train/test splits.
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+    """
+    X = data[features]
+    y = preprocessing.LabelEncoder().fit_transform(data[classes])
+
+    cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5))
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
     plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
              label="Training score")
     plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
